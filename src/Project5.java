da5fd5e8f8a30d75578852aa56521e74e48d4a38
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-29 17:29:12 -0400

    Client and Server classes

--- src/Project5.java
+++ src/Project5.java
@@ -1,6 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 public class Project5 {
     public static void main(String[] args) {
-        System.out.println("Hello Dhanush");
+        Socket socket = new Socket();
+        Server server = new Server(socket);
+        try {
+            server.serverStart();
+        } catch (IOException e) {
+            System.out.println("Server Start IOException");
+        }
+        
     }
 
 }
