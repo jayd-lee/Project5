87449a3c19a94f7f3d8395bf5b0c7bde148d7dc6
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-29 18:49:58 -0400

    WORKING SERVER AND CLIENT

--- src/Client.java
+++ src/Client.java
@@ -1,44 +1,40 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
         // create socket on agreed upon port (and local host for this example)...
-        Client client = new Client();
         Socket socket = null;
         try {
-            socket = new Socket("", 4343);
+            socket = new Socket("localhost", 4343);
+            System.out.println("Connection successful");
         } catch (ConnectException e) {
             System.out.println("Connection failed");
         }
-        Server server = new Server(socket);
-        boolean serverstatus = client.serverStatus();
-        if (!serverstatus) {
-            server.serverStart();
-        }
-        // open input stream first, gets header from server...
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        // open output stream second, send header to server...
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        oos.flush(); // ensure data is sent to the server
-        // read object(s) from server...
-        String s1 = (String) ois.readObject();
-        System.out.printf("received from server: %s\n", s1);
-        // write object(s) to server...
-        String s2 = s1.toUpperCase();
-        oos.writeObject(s2);
-        oos.flush(); // ensure data is sent to the server
-        System.out.printf("sent to server: %s\n", s2);
-        // close streams...
-        oos.close();
-        ois.close();
-    }
-    public boolean serverStatus() {
-        try (Socket socket = new Socket("dmanjun@purdue.edu", 4343)) {
-            return true;
-        } catch (IOException e) {
-            System.out.println("Server Offline");
-        }
-        return false;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is ur name?");
+        String name = scan.nextLine();
+        System.out.println("What is ur home address?");
+        String address = scan.nextLine();
+        System.out.println("What is ur social security number?");
+        int ssn = scan.nextInt();
+        scan.nextLine();
+        // // open input stream first, gets header from server...
+        // ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        // // open output stream second, send header to server...
+        // ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        // oos.flush(); // ensure data is sent to the server
+        // // read object(s) from server...
+        // String s1 = (String) ois.readObject();
+        // System.out.printf("received from server: %s\n", s1);
+        // // write object(s) to server...
+        // String s2 = s1.toUpperCase();
+        // oos.writeObject(s2);
+        // oos.flush(); // ensure data is sent to the server
+        // System.out.printf("sent to server: %s\n", s2);
+        // // close streams...
+        // oos.close();
+        // ois.close();
     }
 }
