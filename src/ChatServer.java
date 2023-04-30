adfabce6af7bb1724cbc44d54c73a433264660cb
AadiG1 <129692966+AadiG1@users.noreply.github.com>
2023-04-30 19:23:07 -0400

    Create ChatServer.java

---
+++ src/ChatServer.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ChatServer {
+    private ServerSocket serverSocket;
+    private static final List<ChatServer.ClientHandler> clients = new LinkedList<>();
+
+    public ChatServer(int port) {
+        try {
+
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server listening on port " + port);
+
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected from " + clientSocket.getInetAddress().getHostAddress());
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader reader;
+        private BufferedWriter writer;
+
+        public ClientHandler(Socket socket) {
+            clientSocket = socket;
+            try {
+
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("Received message from " + clientSocket.getInetAddress().getHostAddress() + ": " + message);
+                    broadcastMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    reader.close();
+                    writer.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void broadcastMessage(String message) {
+            for (ClientHandler client : clients) {
+                if (client != this) {
+                    try {
+                        client.writer.write(message + "\n");
+                        client.writer.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int port = 12345;
+        new ChatServer(port);
+    }
+}
+
