7be96e1e4716ee52f418d2b6394859c6cd2a45dd
AadiG1 <129692966+AadiG1@users.noreply.github.com>
2023-04-30 17:47:18 -0400

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -4,34 +4,36 @@ import java.util.*;
 
 // Server class
 class Server {
+    private static final List<ClientHandler> clients = new LinkedList<>();
+    private static int clientCount = 1;
+
     public static void main(String[] args) {
-        ServerSocket server = null;
+        ServerSocket s = null;
 
         try {
             // server is listening on port 1234
-            server = new ServerSocket(1234);
-            server.setReuseAddress(true);
+            s = new ServerSocket(1234);
+            s.setReuseAddress(true);
 
             // running infinite loop for getting client request
             while (true) {
-                Socket client = server.accept();
+                Socket client = s.accept();
 
                 // Displaying that new client is connected to server
                 System.out.println("New client connected" + client.getInetAddress().getHostAddress());
 
-                // create a new thread object
-                Thread t = new Thread(new ClientHandler(client));
-
-                // This thread will handle the client
-                // separately
-                t.start();
+                // create a new thread to handle the client
+                ClientHandler ch = new ClientHandler(client);
+                clients.add(ch);
+                ch.start();
+                clientCount++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (server != null) {
+            if (s != null) {
                 try {
-                    server.close();
+                    s.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -39,48 +41,56 @@ class Server {
         }
     }
 
-    private static class ClientHandler implements Runnable {
+    private static class ClientHandler extends Thread {
         private final Socket clientSocket;
-        private static List<Socket> clients = new ArrayList<>();
+        private final PrintWriter out;
+        private final BufferedReader in;
+        private final int clientId;
 
-        public ClientHandler(Socket socket) {
+        // messageHistory stores all messages sent by all clients
+        private static final LinkedList<String> messageHistory = new LinkedList<>();
+
+        public ClientHandler(Socket socket) throws IOException {
             this.clientSocket = socket;
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientId = clientCount;
         }
+
         public void run() {
-            PrintWriter out = null;
-            BufferedReader in = null;
             try {
-                // add this client to the list of connected clients
-                clients.add(clientSocket);
-
-                // get the outputstream of client
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                // get the inputstream of client
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
                 String line;
                 while ((line = in.readLine()) != null) {
-                    System.out.printf(" Sent from the client: %s\n", line);
-
-                    // broadcast the input to all connected clients
-                    for (Socket socket : clients) {
-                        PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
-                        socketOut.println(line);
+                    System.out.printf("Client %d sent: %s\n", clientId, line);
+                    // add message to messageHistory
+                    messageHistory.add("Client " + clientId + ": " + line);
+                    // broadcast message to all clients except the original sender
+                    synchronized (clients) {
+                        for (ClientHandler handler : clients) {
+                            if (handler.clientId != clientId) {
+                                handler.out.printf("Client %d: %s\n", clientId, line);
+                            }
+                        }
+                    }
+                    // send repeated message to original sender only
+                    int count = 0;
+                    for (String msg : messageHistory) {
+                        if (msg.equals("Client " + clientId + ": " + line)) {
+                            count++;
+                        }
+                    }
+                    if (count > 1) {
+                        out.printf("Client %d: %s\n", clientId, line);
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    if (out != null) {
-                        out.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                        clientSocket.close();
-                        // remove this client from the list of connected clients
-                        clients.remove(clientSocket);
-                    }
+                    out.close();
+                    in.close();
+                    clientSocket.close();
+                    clients.remove(this);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
